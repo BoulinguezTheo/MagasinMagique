da9d22afbb29f76234d0a2952b1e34118545ae15
Theo Boulinguez <thoe.boulinguez@le-campus-numerique.fr>
2023-03-24 10:45:12 +0100

    tests and refactor

--- src/main/java/com/magasin/Magasin.java
+++ src/main/java/com/magasin/Magasin.java
@@ -8,55 +8,194 @@ class Magasin {
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Comté")
-                    && !items[i].name.equals("Pass VIP Concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Kryptonite")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+        for (Item item : items) {
+            updateItemQuality(item);
+        }
+    }
 
-                    if (items[i].name.equals("Pass VIP Concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
+    private void updateItemQuality(Item item) {
+        if (item.name.equals("Comté")) {
+            updateComteQuality(item);
+        } else if (item.name.equals("Pass VIP Concert")) {
+            updatePassVipConcertQuality(item);
+        } else if (!item.name.equals("Kryptonite")) {
+            updateNormalItemQuality(item);
+        }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
+        if (!item.name.equals("Kryptonite")) {
+            item.sellIn -= 1;
+        }
+
+        if (item.sellIn < 0) {
+            updateItemQualityWhenSellInPassed(item);
+        }
+    }
+
+    private void updateComteQuality(Item item) {
+        if (item.quality < 50) {
+            item.quality += 1;
+        }
+    }
+
+    private void updatePassVipConcertQuality(Item item) {
+        if (item.quality < 50) {
+            item.quality += 1;
+            if (item.sellIn < 11) {
+                item.quality += 1;
             }
-
-            if (!items[i].name.equals("Kryptonite")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Comté")) {
-                    if (!items[i].name.equals("Pass VIP Concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Kryptonite")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
+            if (item.sellIn < 6) {
+                item.quality += 1;
             }
         }
     }
+
+    private void updateNormalItemQuality(Item item) {
+        if (item.quality > 0) {
+            item.quality -= 1;
+        }
+    }
+
+    private void updateItemQualityWhenSellInPassed(Item item) {
+        if (item.name.equals("Comté")) {
+            updateComteQuality(item);
+        } else if (item.name.equals("Pass VIP Concert")) {
+            item.quality = 0;
+        } else {
+            updateNormalItemQuality(item);
+        }
+    }
+/* ***************MOI******************* */
+
+//    public void updateQuality() {
+//        for (int i = 0; i < items.length; i++) {
+//            updateQualityItemsWhileValid(i);
+//            updateSellInItems(i);
+//            updateQualityItemsWhileExpired(i);
+//        }
+//    }
+
+    private void updateQualityItemsWhileValid(int i) {
+        if (!items[i].name.equals("Comté")
+                && !items[i].name.equals("Pass VIP Concert")) {
+            updateQualityBasicItems(i);
+        } else {
+            if (items[i].quality < 50) {
+                updateQualityBy1(i);
+                updateQualityConcerts(i);
+            }
+        }
+    }
+
+    private void updateQualityBasicItems(int i){
+        if (items[i].quality > 0 && !items[i].name.equals("Kryptonite")) {
+                items[i].quality = items[i].quality - 1;
+        }
+    }
+
+    private void updateQualityBy1(int i) {
+        items[i].quality = items[i].quality + 1;
+    }
+    private void updateQualityConcerts(int i) {
+        if (items[i].name.equals("Pass VIP Concert")
+                && items[i].quality < 50) {
+            updateQualityConcertBy2(i);
+            updateQualityConcertBy3(i);
+        }
+    }
+
+    private void updateQualityConcertBy2(int i) {
+        int daysBeforeExpire = 11;
+        if (items[i].sellIn < daysBeforeExpire) {
+            items[i].quality = items[i].quality + 1;
+        }
+    }
+
+    private void updateQualityConcertBy3(int i) {
+        int daysBeforeExpire = 6;
+        if (items[i].sellIn < daysBeforeExpire) {
+            items[i].quality = items[i].quality + 1;
+        }
+    }
+
+    private void updateSellInItems(int i) {
+        if (!items[i].name.equals("Kryptonite")) {
+            items[i].sellIn = items[i].sellIn - 1;
+        }
+    }
+
+    private void updateQualityItemsWhileExpired(int i) {
+        if (items[i].sellIn < 0) {
+            if (!items[i].name.equals("Comté") && !items[i].name.equals("Pass VIP Concert")) {
+                updateQualityBasicItems(i);
+            } else if (items[i].name.equals("Pass VIP Concert")) {
+                setQualityToZero(i);
+            }
+            updateQualityComte(i);
+        }
+
+    }
+
+    private void setQualityToZero(int i) {
+        items[i].quality = items[i].quality - items[i].quality;
+    }
+
+    private void updateQualityComte(int i) {
+        if(items[i].name.equals("Comté") && items[i].quality < 50) {
+            updateQualityBy1(i);
+        }
+
+    }
+
+//    public void updateQuality() {
+//        for (int i = 0; i < items.length; i++) {
+//            if (!items[i].name.equals("Comté")
+//                    && !items[i].name.equals("Pass VIP Concert")) {
+//                if (items[i].quality > 0) {
+//                    if (!items[i].name.equals("Kryptonite")) {
+//                        items[i].quality = items[i].quality - 1;
+//                    }
+//                }
+//            } else {
+//                if (items[i].quality < 50) {
+//                    items[i].quality = items[i].quality + 1;
+//
+//                    if (items[i].name.equals("Pass VIP Concert")) {
+//                        if (items[i].sellIn < 11) {
+//                            if (items[i].quality < 50) {
+//                                items[i].quality = items[i].quality + 1;
+//                            }
+//                        }
+//
+//                        if (items[i].sellIn < 6) {
+//                            if (items[i].quality < 50) {
+//                                items[i].quality = items[i].quality + 1;
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//
+//            if (!items[i].name.equals("Kryptonite")) {
+//                items[i].sellIn = items[i].sellIn - 1;
+//            }
+//
+//            if (items[i].sellIn < 0) {
+//                if (!items[i].name.equals("Comté")) {
+//                    if (!items[i].name.equals("Pass VIP Concert")) {
+//                        if (items[i].quality > 0) {
+//                            if (!items[i].name.equals("Kryptonite")) {
+//                                items[i].quality = items[i].quality - 1;
+//                            }
+//                        }
+//                    } else {
+//                        items[i].quality = items[i].quality - items[i].quality;
+//                    }
+//                } else {
+//                    if (items[i].quality < 50) {
+//                        items[i].quality = items[i].quality + 1;
+//                    }
+//                }
+//            }
+//        }
+//    }
 }
--- src/test/java/com/magasin/MagasinTest.java
+++ src/test/java/com/magasin/MagasinTest.java
@@ -2,16 +2,123 @@ package com.magasin;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 class MagasinTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    void testItems() {
+        Random random = new Random();
+        int peremption = random.nextInt(100 - 0 + 1) + 0;
+        int quality = random.nextInt(100 - 0 + 1) + 0;
+        Item[] items = new Item[] { new Item("yaourt", peremption, quality)};
+        Magasin app = new Magasin(items);
+        // WHEN
+        app.updateQuality();
+        // THEN
+        assertsItems(peremption, quality, items);
+    }
+
+    private void assertsItems(int peremption, int quality, Item[] items) {
+        if (peremption > 0) {
+            /* sellIn update */
+            assertEquals(peremption - 1, items[0].sellIn);
+            assertEquals(quality - 1, items[0].quality);
+        } else if (peremption < 0) {
+            /* quality update */
+            assertEquals(quality - 2, items[0].quality);
+            assertEquals(peremption, items[0].sellIn);
+        }
+    }
+
+    @Test
+    void testConcertBeforeSellIn() {
+        Item[] items = new Item[]{new Item("Pass VIP Concert", 15, 20)};
         Magasin app = new Magasin(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertConcertBeforeSellIn(15, 20, items);
+    }
+
+    @Test
+    void testConcertWithin10Days() {
+        Item[] items = new Item[]{new Item("Pass VIP Concert", 10, 20)};
+        Magasin app = new Magasin(items);
+        app.updateQuality();
+        assertConcertWithin10Days(10, 20, items);
+    }
+
+    @Test
+    void testConcertWithin5Days() {
+        Item[] items = new Item[]{new Item("Pass VIP Concert", 5, 20)};
+        Magasin app = new Magasin(items);
+        app.updateQuality();
+        assertConcertWithin5Days(5, 20, items);
+    }
+
+    @Test
+    void testConcertAfterSellIn() {
+        Item[] items = new Item[]{new Item("Pass VIP Concert", -1, 20)};
+        Magasin app = new Magasin(items);
+        app.updateQuality();
+        assertConcertAfterSellIn(-1, 20, items);
+    }
+
+    private void assertConcertBeforeSellIn(int sellIn, int quality, Item[] items) {
+        assertEquals(quality + 1, items[0].quality);
+        assertEquals(sellIn - 1, items[0].sellIn);
+    }
+
+    private void assertConcertWithin10Days(int sellIn, int quality, Item[] items) {
+        assertEquals(quality + 2, items[0].quality);
+        assertEquals(sellIn - 1, items[0].sellIn);
+    }
+
+    private void assertConcertWithin5Days(int sellIn, int quality, Item[] items) {
+        assertEquals(quality + 3, items[0].quality);
+        assertEquals(sellIn - 1, items[0].sellIn);
+    }
+
+    private void assertConcertAfterSellIn(int sellIn, int quality, Item[] items) {
+        assertEquals(0, items[0].quality);
+        assertEquals(sellIn - 1, items[0].sellIn);
+    }
+
+
+    @Test
+    void testKrypto() {
+        Random random = new Random();
+        int peremption = random.nextInt(100 - 0 + 1) + 0;
+        int quality = 50;
+        Item[] items = new Item[] { new Item("Kryptonite", peremption, quality)};
+        Magasin app = new Magasin(items);
+        // WHEN
+        app.updateQuality();
+        // THEN
+        /* sellIn update */
+        assertEquals(peremption, items[0].sellIn);
+        /* quality update */
+        assertEquals(quality, items[0].quality);
+    }
+
+    @Test
+    void testComteSellIn() {
+        int preremption = 0;
+        int quality = 50;
+        Item[] items = new Item[] { new Item("Comté", preremption, quality)};
+        Magasin app = new Magasin(items);
+        // WHEN
+        app.updateQuality();
+        // THEN
+        /* sellIn update */
+        assertEquals(preremption - 1, items[0].sellIn);
+        /* quality update */
+        assertEquals(quality, items[0].quality);
     }
 
 }
